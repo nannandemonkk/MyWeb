70e0a67b75740bd8d209c310a3c5ba6c9b029270
nannan <nannandemonking@163.com>
2026-05-13 20:03:33 +0800

    字体

--- src/pages/1.jsx
+++ src/pages/1.jsx
@@ -4,7 +4,9 @@ import reactLearning from "../document/1.md?raw";
 export default function Post1() {
   return (
     <div className="container content">
-      <ReactMarkdown>{reactLearning}</ReactMarkdown>
+      <div className="post">
+        <ReactMarkdown>{reactLearning}</ReactMarkdown>
+        </div>
     </div>
   );
 }
